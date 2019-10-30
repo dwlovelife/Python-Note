a30156b2679e3c077939225e9090044b93e5e0f0
dwlovelife <dwlovelife@126.com>
2019-10-30 16:12:41 +0800

    改变目录结构

---
+++ code/basic/day01/firstPython.py
@@ -0,0 +1,11 @@
+"""
+第一个python程序
+author:coke
+"""
+name = "coke"
+#信息为虚构
+phoneNum = "17315211630"
+email = "dwlove@162.com"
+print("="*11)
+print("姓名:%s\n手机号码:%s\n邮箱地址:%s"%(name,phoneNum,email))
+print("="*11)
